4aad495b175867833832342a16e3bf45fb1276cc
Victor Luz <85527775+vitinop@users.noreply.github.com>
2021-12-08 05:18:05 -0300

    Updates - Mobile
    ainda falta estilização Desktop do Header/Footer, adição de cores, acertar o posicionamento exato de elementos.

--- MetaStore-Project/src/pages/Home/Home.js
+++ MetaStore-Project/src/pages/Home/Home.js
@@ -1,23 +1,22 @@
-import Slider from '../../components/Slider/Slider';
-import './Home.css';
-import { FiFacebook } from 'react-icons/fi';
-import { FaInstagram, FaWhatsapp } from 'react-icons/fa';
-import { MdOutlineEmail } from 'react-icons/md';
+import Slider from "../../components/Slider/Slider";
+import "./Home.css";
+import { FiFacebook } from "react-icons/fi";
+import { FaInstagram, FaWhatsapp } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
 
+export default function Home() {
+  return (
+    <div classname="home">
+      <Slider />
 
+      <div className="midias">
+        <FiFacebook />
+        <FaInstagram />
+        <MdOutlineEmail />
+        <FaWhatsapp />
+      </div>
 
-export default function Home(){
-    return(
-        <div classname='home'>
-            <Slider />
-            <div className='midias'>
-                <FiFacebook />
-                <FaInstagram />
-                <MdOutlineEmail />
-                <FaWhatsapp />
-            </div>
-
-            <span>Direitos autorais metastore&copy; - 2021 </span>
-        </div>
-    )
+      <span>Direitos autorais metastore&copy; - 2021 </span>
+    </div>
+  );
 }
