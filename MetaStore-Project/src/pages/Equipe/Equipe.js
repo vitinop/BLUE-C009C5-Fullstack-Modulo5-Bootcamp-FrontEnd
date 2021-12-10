5b159a7255b71d9db5313ba5e2e499d7fc890fff
marlacabral <roncolicabral@gmail.com>
2021-12-10 01:26:56 -0300

    inclusão do componente equipe

---
+++ MetaStore-Project/src/pages/Equipe/Equipe.js
@@ -0,0 +1,13 @@
+import './Equipe.css';
+
+export default function Equipe() {
+
+    return(
+        <div className='equipe-father'>
+            <div className='equipe-marla'>
+                <img src='./pages/Equipe/1.jpg' alt='marla'/>
+            </div>
+        </div>
+    )
+    
+}
