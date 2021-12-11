3304094a743c80bbf482009ad531c0f7c842863a
marlacabral <roncolicabral@gmail.com>
2021-12-11 01:28:53 -0300

    inclusão dos componentes friends, termos de uso, lingua de sinais, equipe

---
+++ MetaStore-Project/src/pages/Friends/Friends.js
@@ -0,0 +1,19 @@
+import './Friends.css';
+import { RiTeamLine } from 'react-icons/ri'
+
+export default function Friends() {
+    return(
+        <div className='friends-father'>
+            <p>Enter the metastore and win 10% off</p>
+
+            <RiTeamLine />
+
+            <h4>HQVLM84TT2</h4>
+
+            <p>Share your code with your friends. They get 15% off the first purchase and you get 10%</p>
+            
+
+        </div>
+    )
+    
+}
