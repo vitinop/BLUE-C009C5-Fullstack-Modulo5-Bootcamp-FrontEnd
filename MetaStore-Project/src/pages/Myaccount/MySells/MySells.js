dd4b5f24787927a681033a033822cc09481eddb6
Victor Luz <85527775+vitinop@users.noreply.github.com>
2021-12-16 19:13:00 -0300

    updates no painel sells

--- MetaStore-Project/src/pages/Myaccount/MySells/MySells.js
+++ MetaStore-Project/src/pages/Myaccount/MySells/MySells.js
@@ -1,17 +1,27 @@
-import './MySells.css'
+import "./MySells.css";
+import { BsPlus } from "react-icons/bs";
 
 export default function MySells() {
-    return(
+  return (
     <div className="center-myaccount">
-        My sells
-        <div className="solditems"></div>
-        <div className="itemPicture">
-          <img
-            src="https://www.viewhotels.jp/ryogoku/wp-content/uploads/sites/9/2020/03/test-img.jpg"
-            alt="item's ProfilePicture"
-          ></img>
+      My sells
+      <div className="itemSold">
+        <div className="itemName">
+          Item Name
+          <div className="itemPicture">
+            <img
+              src="https://www.viewhotels.jp/ryogoku/wp-content/uploads/sites/9/2020/03/test-img.jpg"
+              alt="item's ProfilePicture"
+            ></img>
+          </div>
+          <BsPlus/>
         </div>
+        <div className="itemInformation">
+          <div className="itemValue">
+            U$D XXX,XX
+          </div>
+        </div>
+      </div>
     </div>
-        )
-    
-    }
+  );
+}
