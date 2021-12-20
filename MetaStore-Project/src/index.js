bcad44d6c775e2b5ebf8062ef5fbf56dbe8fdca0
Victor Luz <85527775+vitinop@users.noreply.github.com>
2021-12-20 20:00:52 -0300

    Update index.js

--- MetaStore-Project/src/index.js
+++ MetaStore-Project/src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/footer/Footer";
+import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home.js";
 import Login from "./pages/Login/Login.js";
 import CreateAccount from "./pages/CreateAccount/CreateAccount.js";
