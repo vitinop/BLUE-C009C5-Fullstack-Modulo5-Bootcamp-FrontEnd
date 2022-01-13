9498dab4666dd47bdfb8b38e4773d34dce3fc597
Victor Luz <85527775+vitinop@users.noreply.github.com>
2022-01-12 22:28:53 -0300

    Update CreateAccount.js

--- MetaStore-Project/src/pages/CreateAccount/CreateAccount.js
+++ MetaStore-Project/src/pages/CreateAccount/CreateAccount.js
@@ -1,43 +1,56 @@
 import "./CreateAccount.css";
 import axios from "axios";
 import { useState } from "react";
-import Api from '../../api/api';
+import { useNavigate } from "react-router-dom";
 
 export default function CreateAccount() {
+  const navigate = useNavigate();
   const [name, setName] = useState("");
-  const [nick_name, setNickname] = useState("");
+  const [nickname, setNickname] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirmation, setPasswordConfirmation] = useState("");
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const clearInput = () => {
+    setName('')
+    setEmail('')
+    setPassword('')
+    setPasswordConfirmation('')
+  }
 
-    const user = {
-      average_qualification : '0',
-      banner:'',
-      description:'',
-      email: email,
-      name: name,
-      nick_name: nick_name,
-      password: password,
-      passwordConfirmation: passwordConfirmation,
-      profile_picture: '',
-      role: 'user',
-      seller: true,
-      status:'ativo',
-    };
-
-    console.log(user)
-
-    axios.post("http://localhost:3000/api/user", user).then((response) => console.log(response));
+  const user = {
+    name: name,
+    profile_picture: "https://unileao.edu.br/wp-content/themes/portalv2.0/img/perfil.png",
+    nick_name: nickname,
+    password: password,
+    email: email,
+    status: "Active",
+    seller: true,
+    description: "Empty description",
+    banner: "https://dtechted.com/wp-content/uploads/2021/11/meta-banner.webp",
+    average_qualification: 0,
+    counter_views_store: 0,
+    role: "user"
   };
+  
+ 
+  axios.post('api/users/create', user)
+  .then((response) => {
+    console.log(response)
+    clearInput()
+    alert("user created successfully")
+    navigate('/login')
+
+  })
+  .catch( (err) => console.log(err))
+
+
 
   return (
     <div className="center-CreateAccount">
       <main className="createAccountContainer">
         <h2>Create an account</h2>
-        <form className="register-form" onSubmit={handleSubmit}>
+        <form className="register-form" >
           <div className="input-field-CreateAcc">
             <input
               type="text"
@@ -45,7 +58,8 @@ export default function CreateAccount() {
               id="fullname"
               placeholder="Enter your full name"
               required
-              onChange={(event) => setName(event.target.value)}
+              value={name} 
+              onChange={event => setName(event.target.value)}
             />
           </div>
           <div className="input-field-CreateAcc">
@@ -55,7 +69,8 @@ export default function CreateAccount() {
               id="nickname"
               placeholder="Enter your nickname"
               required
-              onChange={(event) => setNickname(event.target.value)}
+              value={nickname} 
+              onChange={event => setNickname(event.target.value)}
             />
           </div>
           <div className="input-field-CreateAcc">
@@ -65,7 +80,8 @@ export default function CreateAccount() {
               id="e-mail"
               placeholder="Enter your E-mail"
               required
-              onChange={(event) => setEmail(event.target.value)}
+              value={email} 
+              onChange={event => setEmail(event.target.value)}
             />
           </div>
           Password
@@ -76,7 +92,9 @@ export default function CreateAccount() {
               id="password"
               placeholder="Enter Your Password"
               required
-              onChange={(event) => setPassword(event.target.value)}
+              value={password} 
+              onChange={event => setPassword(event.target.value)}
+    
             />
           </div>
           Password Confirmation
@@ -87,7 +105,8 @@ export default function CreateAccount() {
               id="passwordConfirmation"
               placeholder="Confirm the Password"
               required
-              onChange={(event) => setPasswordConfirmation(event.target.value)}
+              value={passwordConfirmation} 
+              onChange={event => setPasswordConfirmation(event.target.value)}
             />
           </div>
           <div className="checkbox-agree">
@@ -102,13 +121,9 @@ export default function CreateAccount() {
               MetaStore Subscriber Agreement?
             </a>
           </div>
-          <button className="save" type="submit">
-            Continue
-          </button>
+          <input type="submit" value="Continue" />
         </form>
       </main>
     </div>
   );
 }
-
-
